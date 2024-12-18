0f58dce58e11f4c8cae68bf9cd5a0b6f4ddb055b
Doan Truong <truong.doan@mor.com.vn>
2024-12-18 11:28:02 +0700

    Remove data_files from chronos2

--- chronos2/dashboard_backend/src/core/utils/config_parser.py
+++ chronos2/dashboard_backend/src/core/utils/config_parser.py
@@ -4,7 +4,8 @@ import json
 class Struct(object):
 
     def __init__(self, data):
-        for name, value in data.items():  # Updated to use items() instead of iteritems()
+        # Updated to use items() instead of iteritems()
+        for name, value in data.items():
             setattr(self, name, self._wrap(value))
 
     def _wrap(self, value):
@@ -13,7 +14,8 @@ class Struct(object):
         else:
             return Struct(value) if isinstance(value, dict) else value
 
-config_path = "./src/data_files/chronos_config.json"
+
+config_path = "./src/core/utils/chronos_config.json"
 
 with open(config_path) as config:
     cfg = json.load(config, object_hook=Struct)
