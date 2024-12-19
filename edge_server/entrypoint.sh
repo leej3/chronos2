70dda77f82869a13f3fa0deda4a528c8a0e63e5a
leej3 <johnleenimh@gmail.com>
2024-12-19 13:17:24 +0000

    fixup

--- edge_server/entrypoint.sh
+++ edge_server/entrypoint.sh
@@ -3,10 +3,11 @@ set -e
 
 socat -d -d PTY,link=/tmp/ptyp0,raw,echo=0 PTY,link=/tmp/ttyp0,raw,echo=0 &
 socat -d -d PTY,link=/tmp/ptyp1,raw,echo=0 PTY,link=/tmp/ttyp1,raw,echo=0 &
-python working-sync-server.py /tmp/ttyp0 &
+# python working-sync-server.py /tmp/ttyp0 &
 
 sleep 2
 echo -e "YES\nt=100" > /tmp/water_in
 echo -e "YES\nt=150" > /tmp/water_out
 
-fastapi run chronos/chronos.py:app --host 0.0.0.0 --port 5171
+
+flask run
