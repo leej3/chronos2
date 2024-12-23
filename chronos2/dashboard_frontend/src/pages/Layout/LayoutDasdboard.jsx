141e62596f0fc7f1823b207377ac9c9363086198
Doan Truong <truong.doan@mor.com.vn>
2024-12-23 14:02:14 +0700

    Feat: update UI , remove top navbar

--- chronos2/dashboard_frontend/src/pages/Layout/LayoutDasdboard.jsx
+++ chronos2/dashboard_frontend/src/pages/Layout/LayoutDasdboard.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import { AppFooter, AppHeader } from '../../components/Header/index';
 import { useNavigate, Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
@@ -17,9 +16,7 @@ const Layout = () => {
   return (
     <div>
       <div className="wrapper d-flex flex-column min-vh-100">
-        <AppHeader />
         <Outlet />
-        <AppFooter />
       </div>
     </div>
   );
