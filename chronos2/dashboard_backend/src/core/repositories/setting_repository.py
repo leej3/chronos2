2b0c87f1d4e4d4d1dab72e05f331195e476209dd
Doan Truong <truong.doan@mor.com.vn>
2025-01-21 09:33:55 +0700

    tests for schuduled process

--- chronos2/dashboard_backend/src/core/repositories/setting_repository.py
+++ chronos2/dashboard_backend/src/core/repositories/setting_repository.py
@@ -4,6 +4,12 @@ from src.core.models import *
 
 
 class SettingRepository:
+    def _get_property_from_db(self, param):
+        param = getattr(Settings, param)
+        with session_scope() as session:
+            (value,) = session.query(param).first()
+        return value
+
     def get_last_settings(self):
         with session_scope() as session:
             settings = session.query(Settings).order_by(desc(Settings.id)).first()
