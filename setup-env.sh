2eaa650d407553011affd7e0c489d4ff2eac7af2
John lee <johnleenimh@gmail.com>
2025-01-23 11:13:19 +0000

    improve .env management (#35)
    * add env setup logic
    
    * use .env local template in ci
    
    * update readme

---
+++ setup-env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Copy all .env.sample files to .env.docker
+for f in $(find . -name .env.sample);
+  do
+    cp "$f" "${f%.sample}.docker"
+  done;
+
+# Copy all .env.sample.local files to .env, this can be useful for running
+# test suites locally that connect to the docker stack
+for f in $(find . -name .env.sample.local);
+  do
+    cp "$f" "${f%.sample.local}"
+  done
