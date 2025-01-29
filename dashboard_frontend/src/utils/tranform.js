8c42b420a13c1ed1519f4f15fc516a8569cd7a0e
leej3 <johnleenimh@gmail.com>
2025-01-29 16:20:50 +0000

    add decimal point to system map temps

--- dashboard_frontend/src/utils/tranform.js
+++ dashboard_frontend/src/utils/tranform.js
@@ -1,4 +1,16 @@
-export const formatNumber = (number, minimumFractionDigits = 2) =>
-  new Intl.NumberFormat([], {
+export const formatNumber = (
+  number,
+  minimumFractionDigits = 1,
+  maximumFractionDigits = 1,
+) => {
+  if (number === null || number === undefined || number === 'N/A') return 'N/A';
+  return new Intl.NumberFormat([], {
     minimumFractionDigits,
+    maximumFractionDigits,
   }).format(number);
+};
+
+export const formatTemperature = (temp) => {
+  if (temp === null || temp === undefined || temp === 'N/A') return 'N/A';
+  return `${formatNumber(temp)}°F`;
+};
