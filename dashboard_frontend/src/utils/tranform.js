1bf1b11ed80ebe699634ea30f21a16c2fe441571
Doan Truong <truong.doan@mor.com.vn>
2025-02-26 09:16:14 +0700

    fix label tooltip chart

--- dashboard_frontend/src/utils/tranform.js
+++ dashboard_frontend/src/utils/tranform.js
@@ -1,3 +1,13 @@
+const format = {
+  timeZone: 'America/Chicago',
+  year: 'numeric',
+  month: 'numeric',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: '2-digit',
+  hour12: false,
+};
+
 export const formatNumber = (
   number,
   minimumFractionDigits = 1,
@@ -17,13 +27,14 @@ export const formatTemperature = (temp) => {
 
 export const formatDate = (date) => {
   if (date === null || date === undefined || date === 'N/A') return 'N/A';
-  return new Date(date).toLocaleString('en-US', {
-    timeZone: 'America/Chicago',
-    year: 'numeric',
-    month: 'numeric',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: '2-digit',
-    hour12: true,
-  });
+  return new Date(date).toLocaleString('en-US', format);
 };
+
+export const formatTimeChicagoByTimestamp = (timestamp) => {
+  return new Intl.DateTimeFormat('en-US', {
+    ...format,
+  }).format(new Date(timestamp * 1000));
+};
+
+export const convertUCTtoUnixTimestamp = (dateStr) =>
+  Math.floor(new Date(dateStr).getTime() / 1000);
