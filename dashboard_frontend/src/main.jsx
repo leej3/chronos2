20058c366bc0e026cec78532d1a4e97349cba7be
leej3 <johnleenimh@gmail.com>
2025-02-27 10:37:05 +0000

    adjust bg color configuration

--- dashboard_frontend/src/main.jsx
+++ dashboard_frontend/src/main.jsx
@@ -8,6 +8,13 @@ import { ToastContainer } from 'react-toastify';
 import { store } from './app/store.js';
 import App from './App.jsx';
 import './index.css';
+
+// Set the background color from environment variable if available
+document.documentElement.style.setProperty(
+  '--vite-background-color',
+  import.meta.env.VITE_BACKGROUND_COLOR || null,
+);
+
 createRoot(document.getElementById('root')).render(
   <>
     <Provider store={store}>
